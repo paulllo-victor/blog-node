2ca69ec4f8e0df9189664994fbd68f6d0748ccd9
Paulo Victor <paulllo.v.m.s@gmail.com>
2023-06-08 22:33:26 -0300

    dando inicio a mostrar os detalhes de artigo

--- index.js
+++ index.js
@@ -26,7 +26,28 @@ app.use("/",categoriesController)
 app.use("/",articlesController)
 
 app.get("/",(req,res)=>{
-    res.render("index")
+    Article.findAll().then(articles => {
+        res.render("index",{articles})
+    })
+    
+});
+
+app.get("/:slug", (req,res) => {
+    var slug = req.params.slug;
+
+    Article.findOne({
+        where: {
+            slug
+        }
+    }).then(article => {
+        if(article != undefined){
+            res.render("article",{article})
+        }else{
+            res.redirect("/")
+        }
+    }).catch( err => {
+        res.redirect("/")
+    })
 })
 
 app.listen(8080,()=>{
